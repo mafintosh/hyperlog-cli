fc44592b7467d07194d980b1b05aba4fefcec7cd
Mathias Buus <mathiasbuus@gmail.com>
2015-02-23 15:12:53 +0100

    upgrade to latest version

--- bin.js
+++ bin.js
@@ -3,59 +3,78 @@
 var minimist = require('minimist')
 var level = require('level')
 var hyperlog = require('hyperlog')
+var proc = require('child_process')
 var argv = minimist(process.argv.slice(2))
 
 var log = hyperlog(level((argv.path || '.')+'/hyperlog'))
 
-
-var add = function(cmd) {
-
+var spawn = function(path) {
+  if (path.indexOf('@') > -1) {
+    var start = path.split(':')[0]
+    var end = path.split(':')[1]
+    return proc.spawn('ssh', ['-o', 'UserKnownHostsFile=/dev/null', '-o', 'StrictHostKeyChecking=no', start, 'hyperlog sync --path '+end])
+  }
+  return proc.spawn('hyperlog', ['sync', '--path', path])
 }
 
-var sync = function(path) {
-  var remote = hyperlog(level(path+'/hyperlog')).replicate()
-  console.log('fetching changes ...')
-  remote.pipe(log.replicate()).pipe(remote).on('finish', function() {
-    console.log('resolving deltas ...')
-    log.resolve()
+var pull = function(path) {
+  var remote = spawn(path)
+  remote.stdout.pipe(log.createReplicationStream({mode: 'pull'})).pipe(remote.stdin)
+}
+
+var push = function(path) {
+  var remote = spawn(path)
+  remote.stdout.pipe(log.createReplicationStream({mode: 'push'})).pipe(remote.stdin)
+}
+
+var onnode = function(node) {
+  console.log('hash  : %s', node.hash)
+  console.log('value : %s', node.value.toString())
+
+  var indent = 'links : '
+  node.links.forEach(function(ln) {
+    console.log(indent+ln.hash)
+    indent = indent.replace(/./g, ' ')
   })
+
+  console.log()
 }
 
 var print = function() {
-  log.changes({reverse:true}).on('data', function(node) {
-    console.log('key   : %s', node.key)
-    console.log('value : %s', node.value.toString())
-
-    var indent = 'links : '
-    node.links.forEach(function(ln) {
-      console.log(indent+ln)
-      indent = indent.replace(/./g, ' ')
-    })
-
-    console.log()
-  })
+  log.createChangesStream({reverse:true}).on('data', onnode)
 }
 
 var heads = function() {
   log.heads().on('data', function(head) {
-    console.log(head)
+    console.log(head.hash)
   })
 }
 
 switch (argv._[0]) {
-  case 'add':
-  log.heads(function(err, nodes) {
+  case 'get':
+  log.get(argv._[1], function (err, node) {
     if (err) throw err
-    log.add(nodes, argv._.slice(1).join(' ') || '(no value)')
+    onnode(node)
+  })
+  return
+
+  case 'add':
+  log.add(argv.link || argv.l, argv.value || argv.v || '(no value)', function (err, node) {
+    if (err) throw err
+    onnode(node)
   })
   return
 
   case 'sync':
-  sync(argv._[1])
+  process.stdin.pipe(log.createReplicationStream()).pipe(process.stdout)
   return
 
-  case 'print':
-  print()
+  case 'pull':
+  pull(argv._[1])
+  return
+
+  case 'push':
+  push(argv._[1])
   return
 
   case 'heads':
@@ -63,4 +82,4 @@ switch (argv._[0]) {
   return
 }
 
-console.log('Unknown command: %s', argv._[0])
+print()
