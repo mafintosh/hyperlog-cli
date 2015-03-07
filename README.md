e9508c3ad1cf2435657f3cb2377da6e2cb5d2c2e
Mathias Buus <mathiasbuus@gmail.com>
2015-03-07 12:27:38 +0100

    upgrade hyperlog

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ npm install -g hyperlog-cli
 a cli thing for [hyperlog](https://github.com/mafintosh/hyperlog)
 
 ```
-hyperlog add hello world
+hyperlog add --value "hello world"
 hyperlog print
 ```
--- bin.js
+++ bin.js
@@ -4,36 +4,88 @@ var minimist = require('minimist')
 var level = require('level')
 var hyperlog = require('hyperlog')
 var proc = require('child_process')
+var http = require('http')
+var https = require('https')
+var duplexify = require('duplexify')
+var url = require('url')
+var stdout = require('single-line-log').stdout
 var argv = minimist(process.argv.slice(2))
+
+var DEFAULT_PORT = 6556
 
 var log = hyperlog(level((argv.path || '.')+'/hyperlog'))
 
 var spawn = function(path) {
+  if (/https?:/.test(path)) {
+    var p = url.parse(path)
+    var req = (p.protocol === 'https' ? https : http).request({method: 'POST', port: p.port || DEFAULT_PORT, host: p.host})
+    var proxy = duplexify()
+
+    req.on('response', function (res) {
+      proxy.setReadable(res)
+      proxy.setWritable(false)
+    })
+
+    return {stdin: req, stdout: proxy}
+  }
   if (path.indexOf('@') > -1) {
     var start = path.split(':')[0]
     var end = path.split(':')[1]
-    return proc.spawn('ssh', ['-o', 'UserKnownHostsFile=/dev/null', '-o', 'StrictHostKeyChecking=no', start, 'hyperlog sync --path '+end])
+    return proc.spawn('ssh', ['-o', 'UserKnownHostsFile=/dev/null', '-o', 'StrictHostKeyChecking=no', start, 'hyperlog replicator --path '+end])
   }
-  return proc.spawn('hyperlog', ['sync', '--path', path])
+  return proc.spawn('hyperlog', ['replicator', '--path', path])
 }
 
-var pull = function(path) {
-  var remote = spawn(path)
-  remote.stdout.pipe(log.createReplicationStream({mode: 'pull'})).pipe(remote.stdin)
+var pulled = 0
+var pushed = 0
+
+var replicate = function (mode) {
+  var stream = log.replicate({mode: mode})
+
+  var print = function () {
+    var msg = ''
+    if (mode === 'pull' || mode === 'sync') msg += 'Pulled ' + pulled + ' nodes\n'
+    if (mode === 'push' || mode === 'sync') msg += 'Pushed ' + pushed + ' nodes\n'
+    stdout(msg)
+  }
+
+  stream.on('pull', function () {
+    pulled++
+    print()
+  })
+
+  stream.on('push', function () {
+    pushed++
+    print()
+  })
+
+  print()
+
+  return stream
 }
 
-var push = function(path) {
+var sync = function (path) {
   var remote = spawn(path)
-  remote.stdout.pipe(log.createReplicationStream({mode: 'push'})).pipe(remote.stdin)
+  remote.stdout.pipe(replicate('sync')).pipe(remote.stdin)
+}
+
+var pull = function (path) {
+  var remote = spawn(path)
+  remote.stdout.pipe(replicate('pull')).pipe(remote.stdin)
+}
+
+var push = function (path) {
+  var remote = spawn(path)
+  remote.stdout.pipe(replicate('push')).pipe(remote.stdin)
 }
 
 var onnode = function(node) {
-  console.log('hash  : %s', node.hash)
+  console.log('key   : %s', node.key)
   console.log('value : %s', node.value.toString())
 
   var indent = 'links : '
   node.links.forEach(function(ln) {
-    console.log(indent+ln.hash)
+    console.log(indent+ln)
     indent = indent.replace(/./g, ' ')
   })
 
@@ -41,12 +93,12 @@ var onnode = function(node) {
 }
 
 var print = function() {
-  log.createChangesStream({reverse:true}).on('data', onnode)
+  log.createReadStream({reverse:true}).on('data', onnode)
 }
 
 var heads = function() {
   log.heads().on('data', function(head) {
-    console.log(head.hash)
+    console.log(head.key)
   })
 }
 
@@ -65,8 +117,22 @@ switch (argv._[0]) {
   })
   return
 
+  case 'server':
+  var server = http.createServer(function (req, res) {
+    req.pipe(replicate('sync')).pipe(res)
+  })
+
+  server.listen(argv.port || 6556, function () {
+    console.log('Server is listening on port %d', server.address().port)
+  })
+  return
+
+  case 'replicator':
+  process.stdin.pipe(log.replicate()).pipe(process.stdout)
+  return
+
   case 'sync':
-  process.stdin.pipe(log.createReplicationStream()).pipe(process.stdout)
+  sync(argv._[1])
   return
 
   case 'pull':
